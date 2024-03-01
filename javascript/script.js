98c9dde3146b5d639ddb0ecfbb207c4b361cf7cc
LebogangMputle <lebozikalala@gmail.com>
2024-03-01 15:47:25 +0200

    added the javascript code

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+
+    var hamburger = document.querySelector('.hamburger-menu');
+    hamburger.classList.toggle('active');
+}
